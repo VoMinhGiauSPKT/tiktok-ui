ad8d0497f0211d3c1ddbe0c303b7b746aeea1e82
VoMinhGiau <minhgiau702@gmail.com>
2025-08-17 22:03:13 +0700

    làm routers và layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { Fragment } from 'react/jsx-runtime';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import DefaultLayout from './Layout/DefaultLayout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>react app</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route,index) => {
+
+            const Page = route.component;
+
+            let Layout = DefaultLayout;
+
+            if(route.layout){
+              Layout=route.layout;
+            }else if(route.layout === null){
+              Layout=Fragment;
+            }
+
+            return (
+              <Route 
+                key={index} 
+                path={route.path} 
+                element={
+                    <Layout>
+                      <Page/>
+                    </Layout>
+                  }
+                />
+              );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
